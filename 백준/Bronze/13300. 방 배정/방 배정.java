b4ab33b291bcc472b81db914deb03a64fe553a9a
mandarinng <132491875+mandarinng@users.noreply.github.com>
2023-08-24 13:42:54 +0900

    [Bronze II] Title: 방 배정, Time: 104 ms, Memory: 12776 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -9,26 +9,20 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(sc.nextLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int [][] room = new int[2][7];//12개의 방. (학년은1부터 시작하니까 7개로 만듦)
-		for(int i=0; i<N; i++) {
+		int[][] room = new int[2][7];// 12개의 방. (학년은1부터 시작하니까 7개로 만듦)
+		for (int i = 0; i < N; i++) {
 			StringTokenizer student = new StringTokenizer(sc.nextLine());
-			int s = Integer.parseInt(student.nextToken());//성별
-			int y = Integer.parseInt(student.nextToken());//학년
+			int s = Integer.parseInt(student.nextToken());// 성별
+			int y = Integer.parseInt(student.nextToken());// 학년
 			room[s][y]++;
 		}
-		int cnt=0;
-		for(int i=0; i<2; i++) {
-			for(int j=0; j<7; j++) {
-				if(room[i][j] != 0) {
-					if(room[i][j]>K) {
-						cnt += room[i][j]/K + room[i][j]%K;
-					}else {
-						cnt++;
-					}
-				}
+		int cnt = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j < 7; j++) {
+				cnt += room[i][j] / K + room[i][j] % K;
 			}
 		}
-		
+
 		System.out.println(cnt);
 
 	}
