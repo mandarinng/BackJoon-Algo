5ba3233cc851139e343d2e9bdf8b873bc91ea60f
mandarinng <132491875+mandarinng@users.noreply.github.com>
2023-08-29 23:43:44 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 196 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		char [][] arr = new char[N][N];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
