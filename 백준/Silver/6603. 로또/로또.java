89b03a0f5d31de7faaa3d7b53a321afee87859ed
mandarinng <132491875+mandarinng@users.noreply.github.com>
2023-09-03 16:22:59 +0900

    [Silver II] Title: 로또, Time: 204 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,46 +1,48 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	static int[] S;
-	static boolean[] visit;
-	static int k;
-	public static void main(String[] args) throws Exception{
+	
+	public static int k;
+	public static int [] S ;
+	public static boolean [] visit;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		while(true) {
+		while (true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			k = Integer.parseInt(st.nextToken());
-			if(k == 0) {
-				break;
-			}
-			visit = new boolean[k];
+			if(k==0) break;
 			S = new int[k];
-			for(int i = 0; i < k; i++) {
+			visit = new boolean[k];
+			for(int i=0; i<k; i++) {
 				S[i] = Integer.parseInt(st.nextToken());
 			}
-			lotto(0, 0);
+			lotto(0,0);
+            System.out.println();
 			
-			System.out.println();
-		}// end of while
-		
+		}
 	}
-	
-	private static void lotto(int start, int depth) {
+
+	public static void lotto(int start, int depth) {
+		//재귀함수 종료조건
 		if(depth == 6) {
-			for(int i =0; i < k; i++) {
+			for(int i=0; i<k; i++) {
 				if(visit[i]) {
 					System.out.print(S[i]+" ");
 				}
 			}
 			System.out.println();
 		}
-		for(int i = start; i < k; i++) {
-			visit[i] = true;
+		
+		for(int i=start; i<k; i++) {
+			visit[i]=true;
 			lotto(i+1, depth+1);
-			visit[i] = false;
+			visit[i]=false;
 		}
+		
 	}
 	
 }
