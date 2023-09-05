f387ab67ae96fd96a19831fe1f7ba8cba661e242
mandarinng <132491875+mandarinng@users.noreply.github.com>
2023-09-06 00:18:33 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 25788 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static int N;
+	public static int M;
+	public static int [] result;
+	public static StringBuilder sb = new StringBuilder();
+	public static int [] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		result = new int[M];
+		
+		combination(0,0);
+		System.out.println(sb);
+		
+	}
+	public static void combination(int depth,int start) {
+		if(depth==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+			result[depth] = arr[i];
+			combination(depth+1,i);
+			
+		}
+	}
+}
