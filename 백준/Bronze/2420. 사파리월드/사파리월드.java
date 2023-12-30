10f7205e40f10538524c4b321344f7374a212a9c
mandarinng <132491875+mandarinng@users.noreply.github.com>
2023-12-31 03:43:45 +0900

    [Bronze V] Title: 사파리월드, Time: 72 ms, Memory: 11424 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.awt.Point;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+		
+		System.out.println(Math.abs(N-M));
+	}
+
+}
