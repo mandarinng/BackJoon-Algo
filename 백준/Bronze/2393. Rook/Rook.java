a2cc402031e79c0df2f7f347ed578801b56752cb
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-01-02 23:19:53 +0900

    [Bronze V] Title: Rook, Time: 72 ms, Memory: 11180 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.awt.Point;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+	System.out.println("  ___  ___  ___\n" + 
+			"  | |__| |__| |\n" + 
+			"  |           |\n" + 
+			"   \\_________/\n" + 
+			"    \\_______/\n" + 
+			"     |     |\n" + 
+			"     |     |\n" + 
+			"     |     |\n" + 
+			"     |     |\n" + 
+			"     |_____|\n" + 
+			"  __/       \\__\n" + 
+			" /             \\\n" + 
+			"/_______________\\");
+	}
+
+}
