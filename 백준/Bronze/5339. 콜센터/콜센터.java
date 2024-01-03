70a684750cef3dc92bb2609d2f9c4ff7e8d847f5
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-01-04 01:55:10 +0900

    [Bronze V] Title: 콜센터, Time: 68 ms, Memory: 11196 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.awt.Point;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("     /~\\\n" + 
+				"    ( oo|\n" + 
+				"    _\\=/_\n" + 
+				"   /  _  \\\n" + 
+				"  //|/.\\|\\\\\n" + 
+				" ||  \\ /  ||\n" + 
+				"============\n" + 
+				"|          |\n" + 
+				"|          |\n" + 
+				"|          |");
+	}
+
+}
