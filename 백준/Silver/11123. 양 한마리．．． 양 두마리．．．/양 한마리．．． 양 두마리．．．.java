9391d1e8ca5f41ed08c231724eeeb4c8262bdc26
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-01-09 00:27:33 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 196 ms, Memory: 27600 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -1,63 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int H, W;
-    static char[][] grid;
-    static boolean[][] visited;
+	
+	static int H,W, count;
+	static char [][] field;
+	static boolean [][] visited;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int tc=0; tc<T; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			count = 0;
+			
+			field = new char[H][W];
+			visited = new boolean[H][W];
+			
+			for(int i=0; i<H; i++) {
+				String input = br.readLine();
+				for(int j=0;j<W; j++) {
+					field[i][j] = input.charAt(j);
+				}
+			}
+			
+			for(int i=0; i<H; i++) {
+				for(int j=0; j<W; j++) {
+					if(!visited[i][j] && field[i][j] == '#') {
+						dfs(i,j);
+						count++;
+					}
+				}
+			}
+			
+			System.out.println(count);
+			
+			
+		}
+	}
+	public static void dfs(int row, int col) {
+		
+		visited[row][col] = true;
+		
+		int [] dr = {0,1,0,-1};
+		int [] dc = {1,0,-1,0};
+		
+		for(int d=0; d<4; d++) {
+			int nr = row + dr[d];
+			int nc = col + dc[d];
+			if(nr<H && nc<W && nr>=0 && nc>=0 && field[nr][nc] == '#' && !visited[nr][nc]) {
+				visited[nr][nc] = true;
+				dfs(nr, nc);
+			}
+		}
+		
+		
+	}
 
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            H = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-
-            grid = new char[H][W];
-            visited = new boolean[H][W];
-
-            for (int i = 0; i < H; i++) {
-                String row = br.readLine();
-                for (int j = 0; j < W; j++) {
-                    grid[i][j] = row.charAt(j);
-                }
-            }
-
-            int count = 0;
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < W; j++) {
-                    if (!visited[i][j] && grid[i][j] == '#') {
-                        dfs(i, j);
-                        count++;
-                    }
-                }
-            }
-
-            System.out.println(count);
-        }
-    }
-
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (isValid(nx, ny) && !visited[nx][ny] && grid[nx][ny] == '#') {
-                dfs(nx, ny);
-            }
-        }
-    }
-
-    static boolean isValid(int x, int y) {
-        return x >= 0 && x < H && y >= 0 && y < W;
-    }
 }
