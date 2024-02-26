c5ee05f96390f298c6d60c865453dd09373b29fd
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-02-27 00:46:11 +0900

    [Gold V] Title: 경쟁적 전염, Time: 284 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -3,18 +3,18 @@ import java.io.*;
 
 public class Main {
 
-    static int N,K,X,Y,S;
-    static int [][] arr;
+    static int N, K, X, Y, S;
+    static int[][] arr;
     static PriorityQueue<Node> pq;
-    static boolean [][] visited;
+    static boolean[][] visited;
 
-    public static class Node implements Comparable<Node>{
+    public static class Node implements Comparable<Node> {
         int row;
         int col;
         int cost;//바이러스 번호
 
 
-        public Node(int row, int col, int cost){
+        public Node(int row, int col, int cost) {
             super();
             this.col = col;
             this.row = row;
@@ -23,12 +23,13 @@ public class Main {
         }
 
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Node o) {
             return this.cost - o.cost;//낮은 순서대로 pq에 넣기
         }
 
     }
-    public static void main(String[] args) throws  Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -36,27 +37,24 @@ public class Main {
         K = Integer.parseInt(st.nextToken());//K이하의 바이러스 번호
 
         arr = new int[N][N];
-        pq = new PriorityQueue<Node>();
+        pq = new PriorityQueue<Node>();// 바이러스 번호에 따라 우선순위를 갖는 큐
         visited = new boolean[N][N];
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-//                if (arr[i][j] > 0) {
-//                    pq.add(new Node(i, j, arr[i][j]));
-//                    visited[i][j] = true;
-//                }
             }
         }
 
         st = new StringTokenizer(br.readLine());
         S = Integer.parseInt(st.nextToken());//S초 후의 바이러스 번호
-        X = Integer.parseInt(st.nextToken())-1;//행
-        Y = Integer.parseInt(st.nextToken())-1;//열
+        X = Integer.parseInt(st.nextToken()) - 1;//행
+        Y = Integer.parseInt(st.nextToken()) - 1;//열
 
-        while(S-->0){
-            if(arr[X][Y] != 0){
+        //매 초마다 새로 bfs 돌림
+        while (S-- > 0) {
+            if (arr[X][Y] != 0) {//S초가 경과하면 bfs그만
                 break;
             }
             bfs();
@@ -65,12 +63,14 @@ public class Main {
         System.out.println(arr[X][Y]);
 
     }
+    //바이러스 전파
     public static void bfs() {
 
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
 
-        for(int i=0; i<N; i++) {
+        //해당 초에 새로 바이러스가 전파된 지역 pq에 넣기, 방문 처리
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (arr[i][j] != 0) {
                     visited[i][j] = true;
@@ -78,24 +78,24 @@ public class Main {
                 }
             }
         }
+        // 우선순위 큐가 빌 때까지 바이러스 전파
+        while (!pq.isEmpty()) {
 
-            while (!pq.isEmpty()) {
+            Node n = pq.poll();// 우선순위가 가장 높은(바이러스 번호가 가장 낮은) 바이러스 선택
 
-                Node n = pq.poll();
-
-                for (int d = 0; d < 4; d++) {
-                    int nr = n.row + dr[d];
-                    int nc = n.col + dc[d];
-                    //범위내이고, 아직 전파 안됐으면 전파
-                    if (nr < N && nc < N && nr >= 0 && nc >= 0 && arr[nr][nc] == 0) {
-                        arr[nr][nc] = n.cost;
-                        visited[nr][nc] = true;
-                    }
+            for (int d = 0; d < 4; d++) {
+                int nr = n.row + dr[d];
+                int nc = n.col + dc[d];
+                //범위내이고, 아직 전파 안됐으면 전파
+                if (nr < N && nc < N && nr >= 0 && nc >= 0 && arr[nr][nc] == 0) {
+                    arr[nr][nc] = n.cost;
+                    visited[nr][nc] = true;
                 }
-
-
             }
 
 
         }
+
+
     }
+}
