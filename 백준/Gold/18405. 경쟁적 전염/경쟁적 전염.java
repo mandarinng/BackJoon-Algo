373dc0b2ef76d0e92ed6f449f0e0917e1c312d00
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-02-27 02:32:34 +0900

    [Gold V] Title: 경쟁적 전염, Time: 240 ms, Memory: 20200 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -1,101 +1,95 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K, X, Y, S;
+    static int N, K, S, R, C;
     static int[][] arr;
     static PriorityQueue<Node> pq;
-    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
     public static class Node implements Comparable<Node> {
         int row;
         int col;
-        int cost;//바이러스 번호
-
+        int cost; // 바이러스 번호
 
         public Node(int row, int col, int cost) {
-            super();
-            this.col = col;
             this.row = row;
+            this.col = col;
             this.cost = cost;
-
         }
 
+        // 바이러스 번호가 낮은 순서부터 pq에 들어가도록 정렬
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;//낮은 순서대로 pq에 넣기
+            return this.cost - o.cost;
         }
-
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());//N줄에 걸쳐 시험관의 정보
-        K = Integer.parseInt(st.nextToken());//K이하의 바이러스 번호
-
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         arr = new int[N][N];
-        pq = new PriorityQueue<Node>();// 바이러스 번호에 따라 우선순위를 갖는 큐
-        visited = new boolean[N][N];
+        pq = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());//S초 후의 바이러스 번호
-        X = Integer.parseInt(st.nextToken()) - 1;//행
-        Y = Integer.parseInt(st.nextToken()) - 1;//열
-
-        //매 초마다 새로 bfs 돌림
-        while (S-- > 0) {
-            if (arr[X][Y] != 0) {//S초가 경과하면 bfs그만
-                break;
-            }
-            bfs();
-        }
-
-        System.out.println(arr[X][Y]);
-
-    }
-    //바이러스 전파
-    public static void bfs() {
-
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
-        //해당 초에 새로 바이러스가 전파된 지역 pq에 넣기, 방문 처리
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
                 if (arr[i][j] != 0) {
-                    visited[i][j] = true;
                     pq.add(new Node(i, j, arr[i][j]));
                 }
             }
         }
-        // 우선순위 큐가 빌 때까지 바이러스 전파
-        while (!pq.isEmpty()) {
 
-            Node n = pq.poll();// 우선순위가 가장 높은(바이러스 번호가 가장 낮은) 바이러스 선택
+        st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken());//시간
+        R = Integer.parseInt(st.nextToken()) - 1;//행
+        C = Integer.parseInt(st.nextToken()) - 1;//열
 
-            for (int d = 0; d < 4; d++) {
-                int nr = n.row + dr[d];
-                int nc = n.col + dc[d];
-                //범위내이고, 아직 전파 안됐으면 전파
-                if (nr < N && nc < N && nr >= 0 && nc >= 0 && arr[nr][nc] == 0) {
-                    arr[nr][nc] = n.cost;
-                    visited[nr][nc] = true;
+        spreadVirus();//바이러스 확산
+
+        System.out.println(arr[R][C]);
+    }
+
+    public static void spreadVirus() {
+        //S초 만큼만 확산하기
+        for (int time = 0; time < S; time++) {
+            //현재 시각에 전염된 지역은 q에 임시 저장
+            Queue<Node> q = new LinkedList<>();
+            while (!pq.isEmpty()) {
+                Node n = pq.poll();
+                int r = n.row;
+                int c = n.col;
+                int num = n.cost;
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    //범위내에 있고, 아직 확산되지 않은 지역만 확산하기
+                    if (nr < N &&  nc < N && nr >= 0 && nc >= 0 && arr[nr][nc] == 0) {
+                        q.add(new Node(nr, nc, num));
+                        arr[nr][nc] = num;
+                    }
                 }
             }
-
-
+            //해당 시각에 전파된 바이러스 임시큐에서 pq로 옮기기
+            while (!q.isEmpty()) {
+                Node temp = q.poll();
+                pq.add(temp);
+            }
+            //1초 경과
         }
-
-
     }
+
+
 }
