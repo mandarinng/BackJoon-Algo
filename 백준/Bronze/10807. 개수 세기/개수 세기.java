09c7bf6ade3651c0bb88503ddebf1e628dabb548
mandarinng <132491875+mandarinng@users.noreply.github.com>
2024-05-28 10:32:44 +0900

    [Bronze V] Title: 개수 세기, Time: 76 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int [] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        int cnt=0;
+        for(int i : arr){
+            if(i == v){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
