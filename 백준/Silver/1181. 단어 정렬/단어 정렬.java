60e617a17faa746d145fffb29b830e7ed90cd96c
심규리 <132491875+mandarinng@users.noreply.github.com>
2024-09-21 00:05:13 +0900

    [Silver V] Title: 단어 정렬, Time: 280 ms, Memory: 29760 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,45 +2,46 @@
 import java.io.*;
 import java.util.*;
 
-class Word implements Comparable<Word> {
+class Word implements Comparable<Word>{
 	int len;
-	String word;
-
-	public Word(int len, String word) {
+	String w;
+	
+	public Word(int len, String w) {
 		this.len = len;
-		this.word = word;
+		this.w = w;
 	}
 
 	@Override
 	public int compareTo(Word o) {
-		if (this.len == o.len) {
-			return this.word.compareTo(o.word);
+		if(this.len == o.len) {
+			return this.w.compareTo(o.w);
 		}
 		return this.len - o.len;
 	}
 }
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
-		Set<String> wordset = new HashSet<>();
-
+		Set<String> words = new HashSet<>();
+		
 		for (int i = 0; i < n; i++) {
-			wordset.add(br.readLine());
+			words.add(br.readLine());
 		}
 		
 		List<Word> wordList = new ArrayList<>();
-		for(String s : wordset) {
-			wordList.add(new Word(s.length(), s));
-		}
 		
+		for(String w : words) {
+			wordList.add(new Word(w.length(), w));
+		}
 		Collections.sort(wordList);
 		
 		StringBuilder sb = new StringBuilder();
 		for(Word w : wordList) {
-			sb.append(w.word).append("\n");
+			sb.append(w.w).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
