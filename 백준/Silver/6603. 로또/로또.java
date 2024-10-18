5fb96b6475912ba96904262319034223a2cd2aee
심규리 <132491875+mandarinng@users.noreply.github.com>
2024-10-18 23:30:25 +0900

    [Silver II] Title: 로또, Time: 68 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,48 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+//백준 16236 아기 상어
+import java.awt.Point;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static int k;
-	public static int [] S ;
-	public static boolean [] visit;
-
-	public static void main(String[] args) throws IOException {
+	static StringBuilder sb = new StringBuilder();
+	static int n;
+	static int [] nums;
+	static boolean [] visited;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			if(k==0) break;
-			S = new int[k];
-			visit = new boolean[k];
-			for(int i=0; i<k; i++) {
-				S[i] = Integer.parseInt(st.nextToken());
-			}
-			lotto(0,0);
-            System.out.println();
-			
-		}
-	}
+		StringTokenizer st;
 
-	public static void lotto(int start, int depth) {
-		//재귀함수 종료조건
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			
+			n = Integer.parseInt(st.nextToken());
+			if(n == 0) break;
+			
+			nums = new int[n];
+			visited = new boolean[n];
+			
+			for(int i=0; i<n; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
+			}
+			combination(0,0);
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
+	public static void combination(int start, int depth) {
 		if(depth == 6) {
-			for(int i=0; i<k; i++) {
-				if(visit[i]) {
-					System.out.print(S[i]+" ");
+			for(int i=0; i<n; i++) {
+				if(visited[i]) {
+					sb.append(nums[i]).append(" ");
 				}
 			}
-			System.out.println();
+			sb.append("\n");
+			return;
 		}
-		
-		for(int i=start; i<k; i++) {
-			visit[i]=true;
-			lotto(i+1, depth+1);
-			visit[i]=false;
+		//조합
+		for(int i=start; i<n; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				combination(i+1, depth+1);
+				visited[i] = false;
+			}
 		}
-		
 	}
-	
 }
