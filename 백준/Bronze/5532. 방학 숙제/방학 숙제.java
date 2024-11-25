dbc926698e1c632b71e4e3085d98cc948adb6c78
심규리 <132491875+mandarinng@users.noreply.github.com>
2024-11-25 23:39:57 +0900

    [Bronze IV] Title: 방학 숙제, Time: 64 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,18 @@
+//백준 5532 방학 숙제
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        double max = 0;
+        max = Math.max((double)A/(double)C, (double)B / (double)D);
+        max = Math.ceil(max);
+        L -= (int)max;
+        System.out.print(L);
+	}
+}
