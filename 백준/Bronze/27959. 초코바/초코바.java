a63a7bd30a04e852064e37846d18d7444a0686aa
심규리 <132491875+mandarinng@users.noreply.github.com>
2024-12-07 09:16:12 +0900

    [Bronze V] Title: 초코바, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a*100<b?"No":"Yes");
+    }
+}
