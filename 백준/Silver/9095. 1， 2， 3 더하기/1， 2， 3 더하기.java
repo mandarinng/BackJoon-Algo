15cefa92d34afc4763dbf28e524957a892fd6d88
심규리 <132491875+mandarinng@users.noreply.github.com>
2024-12-16 12:03:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
+//백준 9095 1,2,3 더하기
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[11];
-		arr[1] = 1; // 1만드는 방법 1개
-		arr[2] = 2; //2만드는 방법 2개
-		arr[3] = 4; //3만드는 방법 4개
-		//4부터는 이 전 세 개를 더한 값
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(st.nextToken());
+		int n = 0;
+		int [] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
 		for (int i = 4; i <= 10; i++) {
-			arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 		}
-		
-		int T = sc.nextInt();
-		
-		for (int t = 0; t < T; t++) {
-			int n = sc.nextInt();
-			System.out.println(arr[n]);
+		for (int tc = 0; tc < t; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			sb.append(dp[n]).append("\n");
 		}
-
+		System.out.println(sb.toString());
 	}
-
 }
